6874175823665949f198d2645e830d27a423bbbc
hoduc1702 <32093847+hoduc1702@users.noreply.github.com>
2017-11-09 07:54:11 +0700

    chưa test

---
+++ app/src/main/java/hoduc1702/com/drawlerlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package hoduc1702.com.drawlerlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import hoduc1702.com.drawlerlayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/hoduc1702/com/drawlerlayout/MainActivityTest.java
@@ -0,0 +1,10 @@
+package hoduc1702.com.drawlerlayout;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hovan on 11/4/2017.
+ */
+public class MainActivityTest {
+
+}
